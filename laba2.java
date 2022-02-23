3c9ca4a85afdb55ecf57f98eacf19ca90b9f23b7
Zyzfd <vladkalihev8@gmail.com>
2022-02-23 13:05:50 +0400

    main

---
+++ laba2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import greedy.greedy;
+
+public class laba2 {
+    public static void main(String[] args) {
+        int[][] mass = { {5, 8, 6, 1, 9, 5, 2}, {6, 2, 4, 6, 5, 7, 3} };
+        int M = 10;
+        
+    }
+}
