78d6aa4b334d28cd0fad74d15f14e2081d5cbb1d
Zyzfd <vladkalihev8@gmail.com>
2022-02-23 16:31:23 +0400

    greedy

--- laba2.java
+++ laba2.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 import greedy.greedy;
+import dinam.dinam;
 
 public class laba2 {
     public static void main(String[] args) {
         int[][] mass = { {30, 15, 50}, {90, 60, 100} };
         int M = 80;
+
         System.out.println(greedy.greedy(mass, M));
+
+        dinam.dinam(mass, M);
     }
 }
